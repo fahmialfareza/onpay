fb55505ab635cba8fd02b5f486ec0612460c4284
Dino Keylas <dinokeylas@gmail.com>
2019-09-06 22:39:11 +0700

    Bottom Navigation Bar

---
+++ app/src/main/java/com/dinokeylas/onpay/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.dinokeylas.onpay;
+
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+    }
+}
